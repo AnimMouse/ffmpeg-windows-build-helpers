bdce0549e61f2bac5bcdbf735c4808618430a1d1
Anim Mouse <git@animmouse.com>
2026-02-03 01:57:03 +0800

    Try to disable amf on 32 bit

--- cross_compile_ffmpeg.sh
+++ cross_compile_ffmpeg.sh
@@ -2528,7 +2528,7 @@ build_ffmpeg() {
     config_options+=" --extra-libs=-lpthread" # for some reason various and sundry needed this linux native
 
     config_options+=" --extra-cflags=-DLIBTWOLAME_STATIC --extra-cflags=-DMODPLUG_STATIC --extra-cflags=-DCACA_STATIC" # if we ever do a git pull then it nukes changes, which overrides manual changes to configure, so just use these for now :|
-    if [[ $build_amd_amf = n ]]; then
+    if [[ $build_amd_amf = n || $bits_target = "32" ]]; then
       config_options+=" --disable-amf" # Since its autodetected we have to disable it if we do not want it. #unless we define no autodetection but.. we don't.
     else
       config_options+=" --enable-amf" # This is actually autodetected but for consistency.. we might as well set it.
@@ -2710,7 +2710,7 @@ build_ffmpeg_dependencies() {
   build_liblzma # Lzma in FFmpeg is autodetected. Uses dlfcn.
   build_iconv # Iconv in FFmpeg is autodetected. Uses dlfcn.
   build_sdl2 # Sdl2 in FFmpeg is autodetected. Needed to build FFPlay. Uses iconv and dlfcn.
-  if [[ $build_amd_amf = y ]]; then
+  if [[ $build_amd_amf = y && $bits_target != "32" ]]; then
     build_amd_amf_headers
   fi
   if [[ $build_intel_qsv = y && $compiler_flavors != "native" ]]; then # Broken for native builds right now: https://github.com/lu-zero/mfx_dispatch/issues/71
